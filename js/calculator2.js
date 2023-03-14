bd2730ad94c6fbf66be60301b9a30700a9389a0c
HungNT <ngthahung3@gmail.com>
2023-03-14 19:10:03 +0900

    new0314

---
+++ js/calculator2.js
@@ -0,0 +1,10 @@
+function cl(){
+    document.getElementById("result").value = '0';
+}
+
+function input(val){
+    document.getElementById("result").value += val;
+}
+function kq(){
+    document.getElementById("result").value = eval(result.value);
+}
